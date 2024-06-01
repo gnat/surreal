8d9c880fe268722cfd85712ebd5d3f1686390452
Nathaniel Sabanski <sabanski.n@gmail.com>
2024-06-01 11:37:57 -0700

    Version bump to 1.2.0

--- surreal.js
+++ surreal.js
@@ -1,4 +1,4 @@
-// Welcome to Surreal 1.1.9
+// Welcome to Surreal 1.2.0
 // Documentation: https://github.com/gnat/surreal
 // Locality of Behavior (LoB): https://htmx.org/essays/locality-of-behaviour/
 let surreal = (function () {
